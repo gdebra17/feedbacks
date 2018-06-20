fa542bc7c36c788070b0e421fc4e398261f5765e
cdedkt <christophe.delattre@decathlon.com>
2018-06-20 15:18:02 +0200

    feedback by id

--- handlers/getFeedbackByToken.js
+++ handlers/getFeedbackByToken.js
@@ -3,10 +3,14 @@ const feedbacksService = require("./../services/feedbacksService");
 function getFeedbackByToken(request, result) {
   const feedbackToken = request.params.token;
   //console.log("handlers/getFeedbackById:", feedbackToken);
-  return feedbacksService.getFeedbackDetailByToken(feedbackToken)
-    .then((infos) => {
-      result.json(infos);
-    });
+  return feedbacksService.getFeedbackIdByToken(feedbackToken)
+  .then(feedbackId => {
+    //console.log("handlers/feedbackId:", feedbackId);
+    return feedbacksService.getFeedbackDetailById(feedbackId)
+      .then((infos) => {
+        result.json(infos);
+      });
+  })
 }
 
 module.exports = getFeedbackByToken;
