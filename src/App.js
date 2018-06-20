de5c15fefca8729d2d109f546da3192b36713b55
Martin Vielvoye <martin.vielvoye@gmail.com>
2018-06-21 00:47:37 +0200

    added route and a cycling chat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 //import './App.css';
 //import About from "./modules/about/About";
-//import Tchat from "./modules/Tchat/Tchat";
+import Tchat from "./modules/Tchat/Tchat";
+import Tchat0 from "./modules/Tchat/Tchat0";
 import {
   BrowserRouter as Router,
   Route,
@@ -17,16 +18,16 @@ class App extends Component {
   render() {
 
     return (
-      <div>
-
-      <div className="container">
-        {/* <About /> */}
-        {/* <Tchat /> */}
-<Form />
-      </div>
-      </div>
+      <Router>
+        <div>
+            <Route exact path="/" component={Form}/>
+            <Route exact path="/chats" component={Tchat} />
+            <Route exact path="/user" component={Tchat0} />
+          </div>
+      </Router>
     );
   }
 }
+// <Route path="/chats/:id" component={ChatComponent} />
 
 export default App;
