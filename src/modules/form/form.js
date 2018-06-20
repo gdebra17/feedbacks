73970d975c9c0f0f11f8357201799a33cd9b6b48
Martin D <martin.delorme@decathlon.com>
2018-06-20 14:38:49 +0200

    config et package

--- src/modules/form/form.js
+++ src/modules/form/form.js
@@ -50,17 +50,17 @@ export default class Form extends React.Component {
           <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
             <div class="card-body">
               <button type="button" class="btn btn-outline-primary btn-file">
-              <input type="file" accept="image/*" capture="camera" name="file-input"/>
+                <input type="file" accept="image/*" capture="camera" name="file-input"/>
               </button>
-              {/* <div class="input-group mb-3">
-  <div class="custom-file">
-    <input type="file" class="custom-file-input" id="inputGroupFile02" />
-    <label class="custom-file-label" for="inputGroupFile02">Choose file</label>
-  </div>
-  <div class="input-group-append">
-    <span class="input-group-text" id="">Upload</span>
-  </div> */}
-            {/* </div> */}
+              <div class="input-group mb-3">
+                <div class="custom-file">
+                  <input type="file" class="custom-file-input" id="inputGroupFile02" />
+                  <label class="custom-file-label" for="inputGroupFile02">Choose file</label>
+                </div>
+                <div class="input-group-append">
+                  <span class="input-group-text" id="">Upload</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
