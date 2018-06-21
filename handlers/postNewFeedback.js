bbf3f1ed186ceec2a8fa0a061a6a899c0d16a9d5
cdedkt <christophe.delattre@decathlon.com>
2018-06-21 18:05:17 +0200

    upload

--- handlers/postNewFeedback.js
+++ handlers/postNewFeedback.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const multer = require("multer");
 
+const feedbacksService = require("./../services/feedbacksService");
+
 const storage = multer.diskStorage({
   destination: path.join(__dirname, "./../uploads/"),
   filename: function (req, file, callback) {
@@ -9,37 +11,33 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage }).single("photo");
 
-const feedbacksService = require("./../services/feedbacksService");
-
 function postNewFeedback(request, result) {
   console.log("handlers/postNewFeedback:", request.body);
 
-  console.log("postNewFeedback 1:", Date.now());
-  upload(request,result,function(err) {
-          if(err) {
-              return result.end("Error uploading file.");
-          }
-          console.log("postNewFeedback 2:", Date.now());
-          //result.end("File is uploaded");
-      });
-
-  console.log("postNewFeedback 3:", Date.now());
-  const username = request.body.username;
-  const mail = request.body.mail;
-  const path_image_user = request.body.path_image_user;
-  const topic = request.body.topic;
-  const content = request.body.content;
-  const decathlonid = request.body.decathlonid;
-  const photo_url = "";
-
-  return feedbacksService.createNewFeedback(username, mail, path_image_user, topic, content, decathlonid, photo_url)
-  .then(infos => {
-    if (infos.errorMessage) {
-      result.json({status: "error", errorMessage: infos.errorMessage});
-    } else {
-      result.json({status: "succeeded", data: infos});
+  upload(request, result, function(err) {
+    if (err) {
+      result.json({status: "error", errorMessage: "Error uploading file."});
     }
-  })
+    const username = request.body.username;
+    const mail = request.body.mail;
+    const pathImageUser = request.body.path_image_user;
+    const topic = request.body.topic;
+    const content = request.body.content;
+    const decathlonid = request.body.decathlonid;
+    let pathPhoto;
+    if (request.file) {
+      pathPhoto = request.file.filename;
+    }
+    
+    return feedbacksService.createNewFeedback(username, mail, pathImageUser, topic, content, decathlonid, pathPhoto)
+    .then(infos => {
+      if (infos.errorMessage) {
+        result.json({status: "error", errorMessage: infos.errorMessage});
+      } else {
+        result.json({status: "succeeded", data: infos});
+      }
+    })
+  });
 }
 
 module.exports = postNewFeedback;
