f7368ea0798abae9617ed9d77cfa4588c32aa54f
Martin D <martin.delorme@decathlon.com>
2018-06-25 16:04:19 +0200

    tchat0

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const Websocket = require("ws");
-const http = process.env.NODE_ENV === "production" ? require("https") : require("http");
+const http = process.env.NODE_ENV === "production" ? require("http") : require("http");
 const cors = require("cors");
 const handlers = require("./handlers/index");
 
---
+++ src/modules/Tchat/Tchat0.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Sidebar } from "../../containers/Sidebar";
+import { MessagesList } from "../../containers/MessagesList";
+import { AddMessage } from "../../containers/AddMessage";
+import "../../App.css";
+import { closeSocket, openSocket, listenerMessage } from "../../store/sendWs"
+import store from "../../store/store"
+import {loadDiscussion} from "../../store/sendWs"
+
+
+
+
+class Tchat extends Component {
+
+  componentDidMount(){
+    // console.log("I did update")
+    // loadDiscussion(window.location.pathname);
+
+  }
+
+  componentWillUnmount(){
+    console.log("beeen on close did unmount");
+    closeSocket();
+  }
+
+  // <Sidebar />
+  render() {
+    return (
+      <div id="container">
+        <section id="main">
+        <h1>SPORTIF</h1>
+
+          <MessagesList />
+          <AddMessage />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Tchat;
