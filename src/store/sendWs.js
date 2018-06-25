191b5c789abf48d8d69faf9d0ab52a733a589eb1
Martin D <martin.delorme@decathlon.com>
2018-06-25 16:38:56 +0200

    port

--- src/store/sendWs.js
+++ src/store/sendWs.js
@@ -4,7 +4,7 @@ const host = window.location.origin.replace(/^http/, 'ws');
 
 // let websocket;
 
-const websocket = new WebSocket(`${host}:${process.env.NODE_ENV === "production" ? 80 : 8080}`);
+const websocket = new WebSocket(`${host}${process.env.NODE_ENV === "production" ? "" : ":8080"}`);
 // console.log("here is the origin : ", window.location.origin.replace(/^http/,"ws"));
 
 
